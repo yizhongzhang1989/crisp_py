cb185bb4ac4d9de87c991d7257ac9f135bfcad02
Daniel <sanjose.daniel@gmail.com>
2025-05-05 11:54:20 +0200

    Small changes for the dual arm script

--- examples/5_dual_arm.py
+++ examples/5_dual_arm.py
@@ -18,9 +18,10 @@ right_arm = Robot(robot_config=faster_publishing_config, namespace="right")
 left_arm.wait_until_ready()
 right_arm.wait_until_ready()
 
+
 # %%
 print(left_arm.end_effector_pose)
-print(right_arm.joint_values)
+print(right_arm.end_effector_pose)
 
 # %%
 print("Going to home position...")
@@ -31,7 +32,6 @@ right_arm.home()
 
 left_arm.controller_switcher_client.switch_controller("cartesian_impedance_controller")
 right_arm.controller_switcher_client.switch_controller("cartesian_impedance_controller")
-
 
 # %%
 def sync(left_arm, right_arm):
@@ -48,7 +48,21 @@ rate = left_arm.node.create_rate(ctrl_freq)
 t = 0.0
 while t < max_time:
     x, y, z = left_arm.end_effector_pose.translation
-    x += -np.sin(t) * 0.1
+    x += -np.sin(t) * 0.03
     left_arm.set_target(position=[x, y, z])
     rate.sleep()
     t += 1.0 / ctrl_freq
+
+# %%
+
+params = [
+    ("task.k_pos_x", 0.0),
+    ("task.k_pos_y", 0.0),
+    ("task.k_pos_z", 0.0),
+    ("task.k_rot_x", 0.0),
+    ("task.k_rot_y", 0.0),
+    ("task.k_rot_z", 0.0),
+    ("nullspace.stiffness", 0.0),
+    ("nullspace.projector_type", "none"),
+]
+left_arm.cartesian_controller_parameters_client.set_parameters(params)
