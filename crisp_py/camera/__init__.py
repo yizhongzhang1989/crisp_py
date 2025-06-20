33400ff154c5d2cabbc651b517ac2d04e01f4325
Maximilian Dösch <doesch.maximilian@gmail.com>
2025-06-20 14:21:45 +0200

    added lazy imports in camera __init__.py

---
+++ crisp_py/camera/__init__.py
@@ -0,0 +1,4 @@
+"""Initialize the gripper module."""
+
+from crisp_py.camera.camera import Camera  # noqa: D104, F401
+from crisp_py.camera.camera_config import CameraConfig, FrankaCameraConfig  # noqa: D104, F401
