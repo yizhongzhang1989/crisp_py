5d7a84660b3a986e0c95bd99b4048a888d01a126
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-07-04 09:57:17 +0200

    Added sensor classes to listen to sensor values

---
+++ crisp_py/sensors/sensor.py
@@ -0,0 +1,94 @@
+"""Contains objects to create sensor readers, basically objects that subscribe to a data stream topic."""
+
+import threading
+
+import numpy as np
+import rclpy
+from rclpy.node import Node
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.qos import qos_profile_sensor_data
+from std_msgs.msg import Float32MultiArray
+
+from crisp_py.sensors.sensor_config import SensorConfig
+
+
+class Sensor:
+    """Interface for sensor wrapper."""
+
+    THREADS_REQUIRED = 2
+
+    def __init__(
+        self,
+        node: Node | None = None,
+        namespace: str = "",
+        sensor_config: SensorConfig | None = None,
+        spin_node: bool = True,
+    ):
+        """Initialize the gripper client.
+
+        Args:
+            node (Node, optional): ROS2 node to use. If None, creates a new node.
+            namespace (str, optional): ROS2 namespace for the gripper.
+            sensor_config (SensorConfig, optional): Configuration for the sensor.
+            spin_node (bool, optional): Whether to spin the node in a separate thread.
+        """
+        if not rclpy.ok() and node is None:
+            rclpy.init()
+
+        self.config = sensor_config if sensor_config else SensorConfig()
+
+        self.node = (
+            rclpy.create_node(
+                node_name=f"{self.config.name}_listener",
+                namespace=namespace,
+                parameter_overrides=[],
+            )
+            if not node
+            else node
+        )
+
+        self._value: np.ndarray | None = None
+
+        self.node.create_subscription(
+            Float32MultiArray,
+            self.config.data_topic,
+            self._callback_sensor_data,
+            qos_profile_sensor_data,
+            callback_group=ReentrantCallbackGroup(),
+        )
+
+        if spin_node:
+            threading.Thread(target=self._spin_node, daemon=True).start()
+
+    @property
+    def value(self) -> np.ndarray:
+        """Get the latest sensor value."""
+        if self._value is None:
+            raise ValueError("Sensor value is not available yet.")
+        return self._value
+
+    def _spin_node(self):
+        if not rclpy.ok():
+            rclpy.init()
+        executor = MultiThreadedExecutor(num_threads=self.THREADS_REQUIRED)
+        executor.add_node(self.node)
+        while rclpy.ok():
+            executor.spin_once(timeout_sec=0.1)
+
+    def is_ready(self) -> bool:
+        """Check if the sensor has a value."""
+        return self._value is not None
+
+    def wait_until_ready(self, timeout: float = 10.0, check_frequency: float = 10.0):
+        """Wait until the gripper is available."""
+        rate = self.node.create_rate(check_frequency)
+        while not self.is_ready():
+            rate.sleep()
+            timeout -= 1.0 / check_frequency
+            if timeout <= 0:
+                raise TimeoutError("Timeout waiting for sensor to be ready.")
+
+    def _callback_sensor_data(self, msg: Float32MultiArray):
+        """Callback for sensor data."""
+        self._value = np.array(msg.data[:], dtype=np.float32)
---
+++ crisp_py/sensors/sensor_config.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class SensorConfig:
+    """Default sensor configuration."""
+
+    name: str = "sensor"
+    data_topic: str = "sensor_data"
+
+
+@dataclass
+class AnySkinSensor(SensorConfig):
+    """Configuration for the AnySkin sensor broadcasted by a node using the anyskin_ros2 wrapper: https://github.com/danielsanjosepro/anyskin_ros2."""
+
+    name: str = "anyskin_sensor"
+    data_topic: str = "tactile_data"
