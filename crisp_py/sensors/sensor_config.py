1417b37e915d4fd5a031f1b3e6c3d0cbf6a5058e
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-08-10 13:30:17 +0200

    added sensor shape

--- crisp_py/sensors/sensor_config.py
+++ crisp_py/sensors/sensor_config.py
@@ -7,6 +7,8 @@ from dataclasses import dataclass
 class SensorConfig:
     """Default sensor configuration."""
 
+    shape: tuple[int, ...]
+
     name: str = "sensor"
     data_topic: str = "sensor_data"
     max_data_delay: float = 1.0
@@ -16,5 +18,6 @@ class SensorConfig:
 class AnySkinSensorConfig(SensorConfig):
     """Configuration for the AnySkin sensor broadcasted by a node using the anyskin_ros2 wrapper: https://github.com/danielsanjosepro/anyskin_ros2."""
 
+    shape: tuple[int, ...] = (5,)
     name: str = "anyskin_sensor"
     data_topic: str = "tactile_data"
