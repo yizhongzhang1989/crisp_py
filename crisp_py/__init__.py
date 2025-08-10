680a53d6ac98aa0e7eb64589bbfdf4b41b4b3037
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-08-10 18:46:11 +0200

    fix: guide use in case that the ros2 control packages are not installed

--- crisp_py/__init__.py
+++ crisp_py/__init__.py
@@ -5,5 +5,17 @@ try:
 except ImportError:
     print("ROS2 should be installed and sourced!")
 
+try:
+    import control_msgs  # noqa: F401
+
+except ImportError:
+    print(
+        "Could not import control_msgs. Make sure that you installed ros-<ROS_DISTRO>-ros2-control(lers)."
+    )
+    print(
+        "If you are using robostack with pixi, add the following lines to your pixi.toml dependencies:"
+    )
+    print('ros-<ROS_DISTRO>-ros2-control = "*"\nros-<ROS_DISTRO>-ros2-controllers = "*"')
+
 
 __version__ = "1.2.0"
